fab58a48abe11d43f8ef942477b91ca7380bd4ed
Matheus Farias <matheusfarias95@hotmail.com>
2024-03-30 15:41:59 -0300

    preparando os componentes e exibindo na tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import CardTrilha from "./components/CardTrilha"
-
+import './App.css'
 function App() {
-  
+  const ListaTrilhas = [{
+    nomeTrilha: 'Trilha da Lagoinha do Leste',
+    cidade: 'Florianopolis',
+    estado: 'SC',
+    duracao: 180,
+    trajeto: 2,
+    dificuldade: 'iniciante',
+    tipo: 'caminhada / trekking',
+    nomeUsuario: 'Matheus',
+    urlImagem: 'https://images.pexels.com/photos/10158978/pexels-photo-10158978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+
+  }
+  ]
 
   return (
-    <>
-      <CardTrilha/>
-    
-    </>
+    <div className="container">
+      {
+        ListaTrilhas.map((ListaTrilhas, index) => (
+          <CardTrilha dadosTrilha={ListaTrilhas} key={index} />
+        ))
+
+      }
+    </div>
   )
 }
 
--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -1,14 +1,28 @@
-
-function CardTrilha() {
+import * as PropTypes from 'prop-types'
+import './style.css'
+function CardTrilha({ dadosTrilha }) {
     return (
 
-        <>
-            <h1>Card trilha</h1>
-
-
-        </>
+        <div className='card-container'>
+            <h1>{dadosTrilha.nomeTrilha}</h1>
+            <span> {dadosTrilha.cidade} / {dadosTrilha.estado}</span>
+            <img width={200} src={dadosTrilha.urlImagem} halt="imagem trilha" />
+        </div>
     )
 
 }
 
-export default CardTrilha;  
+CardTrilha.PropTypes = {
+    ListaTrilhas: PropTypes.exact({
+        nomeTrilha: PropTypes.string.isRequired,
+        cidade: PropTypes.string.isRequired,
+        estado: PropTypes.string.isRequired,
+        duracao: PropTypes.number.isRequired,
+        trajeto: PropTypes.number.isRequired,
+        dificuldade: PropTypes.string.isRequired,
+        tipo: PropTypes.isRequired,
+        nomeUsuario: PropTypes.string.isRequired,
+        urlImagem: PropTypes.string.isRequired
+    })
+}
+export default CardTrilha;
